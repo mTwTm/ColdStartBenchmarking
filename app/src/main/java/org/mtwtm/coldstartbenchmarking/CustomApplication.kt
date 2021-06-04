7d384d3a06715a16dabfa9fc1610e5b1df5e154d
Teng-pao Yu <git@mtwtm.org>
2021-06-04 17:14:54 +0800

    feat: Store firstDrawMs

--- app/src/main/java/org/mtwtm/coldstartbenchmarking/CustomApplication.kt
+++ app/src/main/java/org/mtwtm/coldstartbenchmarking/CustomApplication.kt
@@ -1,13 +1,66 @@
 package org.mtwtm.coldstartbenchmarking
 
+import android.app.Activity
 import android.app.Application
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.os.SystemClock
+import org.mtwtm.coldstartbenchmarking.coldstart.NextDrawListener.Companion.onNextDraw
+import org.mtwtm.coldstartbenchmarking.coldstart.WindowDelegateCallback.Companion.onDecorViewReady
 import org.mtwtm.coldstartbenchmarking.metadata.ColdStartStats
 
 class CustomApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        var firstDraw = false
+        val handler = Handler(Looper.getMainLooper())
+
+        registerActivityLifecycleCallbacks(
+            object : ActivityLifecycleCallbacks {
+                override fun onActivityCreated(
+                    activity: Activity,
+                    savedInstanceState: Bundle?
+                ) {
+                    if (firstDraw) return
+                    val window = activity.window
+                    window.onDecorViewReady {
+                        window.decorView.onNextDraw {
+                            if (firstDraw) return@onNextDraw
+                            firstDraw = true
+                            handler.postAtFrontOfQueue {
+                                ColdStartStats.firstDrawMs = SystemClock.uptimeMillis()
+                            }
+                        }
+                    }
+                }
+
+                override fun onActivityStarted(activity: Activity) {
+                    // Do nothing.
+                }
+
+                override fun onActivityResumed(activity: Activity) {
+                    // Do nothing.
+                }
+
+                override fun onActivityPaused(activity: Activity) {
+                    // Do nothing.
+                }
+
+                override fun onActivityStopped(activity: Activity) {
+                    // Do nothing.
+                }
+
+                override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {
+                    // Do nothing.
+                }
+
+                override fun onActivityDestroyed(activity: Activity) {
+                    // Do nothing.
+                }
+            })
         ColdStartStats.applicationOnCreateMs = SystemClock.uptimeMillis()
         ColdStartStats.applicationLoadedMs = LOAD_TIME
     }
